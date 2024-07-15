40c1180725b964e6f5bc585025b89b59d5cbd3e2
progIfe <ifeoluwasamson90@gmail.com>
2024-07-15 15:22:29 +0100

    Project initialization and configuration

---
+++ sendcommerce/settings/dev.py
@@ -0,0 +1,20 @@
+from decouple import config
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS += ['localhost', '127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DEV_DB_NAME'),
+        'USER': config('DEV_DB_USER'),
+        'PASSWORD': config('DEV_DB_PASSWORD'),
+        'HOST': config('DEV_DB_HOST'),
+        'PORT': config('DEV_DB_PORT', default='5432'),
+    }
+}
