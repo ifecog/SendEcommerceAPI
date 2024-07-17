d58ac573657468c4e9decdfeb0e3c898341a7d6a
progIfe <ifeoluwasamson90@gmail.com>
2024-07-17 01:25:42 +0100

    Add order related models

--- base/models.py
+++ base/models.py
@@ -58,3 +58,95 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('Credit Card', 'Credit Card'),
+        ('PayPal', 'PayPal')
+    ]
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    payment_method = models.CharField(max_length=200, choices=PAYMENT_METHOD_CHOICES, null=True, blank=True)
+    tax_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(0)])
+    shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(0)])
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(0)])
+    created_time = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    payment_time = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    is_delivered = models.BooleanField(default=False)
+    delivery_time = models.DateTimeField(auto_now_add=False, null=True, blank=True)    
+
+    def __str__(self):
+        return str(self.created_time)
+    
+    
+class OrderItem(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ShippingAddress(models.Model):
+    NIGERIAN_STATES = [
+        ('Abia', 'Abia'),
+        ('Adamawa', 'Adamawa'),
+        ('Akwa Ibom', 'Akwa Ibom'),
+        ('Anambra', 'Anambra'),
+        ('Bauchi', 'Bauchi'),
+        ('Bayelsa', 'Bayelsa'),
+        ('Benue', 'Benue'),
+        ('Borno', 'Borno'),
+        ('Cross River', 'Cross River'),
+        ('Delta', 'Delta'),
+        ('Ebonyi', 'Ebonyi'),
+        ('Edo', 'Edo'),
+        ('Ekiti', 'Ekiti'),
+        ('Enugu', 'Enugu'),
+        ('FCT', 'FCT'),
+        ('Gombe', 'Gombe'),
+        ('Imo', 'Imo'),
+        ('Jigawa', 'Jigawa'),
+        ('Kaduna', 'Kaduna'),
+        ('Kano', 'Kano'),
+        ('Katsina', 'Katsina'),
+        ('Kebbi', 'Kebbi'),
+        ('Kogi', 'Kogi'),
+        ('Kwara', 'Kwara'),
+        ('Lagos', 'Lagos'),
+        ('Nasarawa', 'Nasarawa'),
+        ('Niger', 'Niger'),
+        ('Ogun', 'Ogun'),
+        ('Ondo', 'Ondo'),
+        ('Osun', 'Osun'),
+        ('Oyo', 'Oyo'),
+        ('Plateau', 'Plateau'),
+        ('Rivers', 'Rivers'),
+        ('Sokoto', 'Sokoto'),
+        ('Taraba', 'Taraba'),
+        ('Yobe', 'Yobe'),
+        ('Zamfara', 'Zamfara'),
+    ]
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    state = models.CharField(max_length=200, choices=NIGERIAN_STATES, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    postal_code = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.address
+
+    class Meta:
+        verbose_name = 'shipping address'
+        verbose_name_plural = 'shipping addresses'
--- base/serializers.py
+++ base/serializers.py
@@ -4,9 +4,13 @@ from .models import (
     Category,
     Tag,
     Brand,
-    Product
+    Product,
+    Order,
+    OrderItem,
+    ShippingAddress,
 )
 
+from users.serializers import UserSerializer
 
 class CategorySerializer(serializers.ModelSerializer):
     
@@ -36,3 +40,46 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             'uuid', 'name', 'image_a', 'image_b', 'image_c', 'image_d', 'description', 'price', 'discount_price', 'rating', 'num_of_reviews', 'count_in_stock', 'category', 'brand', 'tags', 'related_products'
         ]
+        
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orderItems(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(
+                obj.shippingaddress, many=False).data
+        except:
+            address = False
+
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+
+        return serializer.data
